f7bdc538c5764815af6f683dff05050fc9b44b9f
Eric Johnson <ericdjohnson@google.com>
2023-01-04 08:19:46 -0800

    Internal change
    PiperOrigin-RevId: 499490062

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ DEPENDENCIES = (
     # specified in the docker container requirements files.
     'astor~=0.8.1',
     'google-auth>=1.17.2',
-    'ipykernel~=5.3.4',
-    'ipython~=7.9.0',
+    'ipykernel~=5.5.6',
+    'ipython~=7.34.0',
     'notebook~=5.7.16',
     'pandas>=1.1.0',
     'portpicker~=1.3.1',
